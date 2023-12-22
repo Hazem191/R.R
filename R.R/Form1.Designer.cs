ba650940aabb57f2c40cfbd56d98d70ed5d7fccf
Hazem Ibrahim <hazem.aldriny@outlook.com>
2023-12-22 15:11:50 +0200

    Add project files.

---
+++ R.R/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace R.R
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtp1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtp2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtp3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtp4 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtq = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(353, 271);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 64);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtp1
+            // 
+            this.txtp1.Location = new System.Drawing.Point(202, 82);
+            this.txtp1.Margin = new System.Windows.Forms.Padding(4);
+            this.txtp1.Name = "txtp1";
+            this.txtp1.Size = new System.Drawing.Size(103, 26);
+            this.txtp1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "P1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "P2";
+            // 
+            // txtp2
+            // 
+            this.txtp2.Location = new System.Drawing.Point(202, 133);
+            this.txtp2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtp2.Name = "txtp2";
+            this.txtp2.Size = new System.Drawing.Size(103, 26);
+            this.txtp2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 169);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "P3";
+            // 
+            // txtp3
+            // 
+            this.txtp3.Location = new System.Drawing.Point(202, 165);
+            this.txtp3.Margin = new System.Windows.Forms.Padding(4);
+            this.txtp3.Name = "txtp3";
+            this.txtp3.Size = new System.Drawing.Size(103, 26);
+            this.txtp3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 201);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "P4";
+            // 
+            // txtp4
+            // 
+            this.txtp4.Location = new System.Drawing.Point(202, 197);
+            this.txtp4.Margin = new System.Windows.Forms.Padding(4);
+            this.txtp4.Name = "txtp4";
+            this.txtp4.Size = new System.Drawing.Size(103, 26);
+            this.txtp4.TabIndex = 4;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.checkedListBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(494, 102);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(181, 142);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(725, 102);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(170, 170);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(555, 60);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "RAM";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(774, 60);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "CPU";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(322, 54);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Q";
+            // 
+            // txtq
+            // 
+            this.txtq.Location = new System.Drawing.Point(365, 54);
+            this.txtq.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq.Name = "txtq";
+            this.txtq.Size = new System.Drawing.Size(103, 26);
+            this.txtq.TabIndex = 5;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.label11.ForeColor = System.Drawing.Color.Blue;
+            this.label11.Location = new System.Drawing.Point(135, 105);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 24);
+            this.label11.TabIndex = 15;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.label12.ForeColor = System.Drawing.Color.Blue;
+            this.label12.Location = new System.Drawing.Point(135, 132);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 24);
+            this.label12.TabIndex = 22;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.label13.ForeColor = System.Drawing.Color.Blue;
+            this.label13.Location = new System.Drawing.Point(135, 164);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 24);
+            this.label13.TabIndex = 21;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.label14.ForeColor = System.Drawing.Color.Blue;
+            this.label14.Location = new System.Drawing.Point(135, 196);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 24);
+            this.label14.TabIndex = 23;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(908, 348);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtq);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtp4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtp3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtp2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtp1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Firebrick;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Simulation R.R";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtp1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtp2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtp3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtp4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtq;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ R.R/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace R.R
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        #region Method
+        public void clear()
+        {
+            label11.Text = "";
+            label12.Text = "";
+            label13.Text = "";
+            label14.Text = "";
+            txtq.Text = txtp1.Text = txtp2.Text = txtp3.Text = txtp4.Text = "";
+            MessageBox.Show("Operation IS Finished ");
+        } 
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label11.Text = txtp1.Text;
+            label12.Text = txtp2.Text;
+            label13.Text = txtp3.Text;
+            label14.Text =txtp4.Text;
+
+            Start();          
+        }
+        private void Start()
+        {
+            if (txtp1.Text != "" && txtp2.Text != "" && txtp3.Text != "" && txtp4.Text != "" && txtq.Text != "")
+            {
+                //p1
+                if (Convert.ToInt16(txtp1.Text) > 0)
+                {
+                    if (Convert.ToInt16(txtp1.Text) >= Convert.ToInt16(txtq.Text))
+                    {
+                        txtp1.Text = (Convert.ToInt16(txtp1.Text) - Convert.ToInt16(txtq.Text)).ToString();
+                        checkedListBox1.Items.Add(label1.Text);
+                    }
+                    else if (Convert.ToInt16(txtp1.Text) < Convert.ToInt16(txtq.Text))
+                    {
+                        txtp1.Text = "0";
+                        checkedListBox1.Items.Add(label1.Text);
+                    }
+                }
+                //p2
+                if (Convert.ToInt16(txtp2.Text) > 0)
+                {
+                    if (Convert.ToInt16(txtp2.Text) >= Convert.ToInt16(txtq.Text))
+                    {
+                        txtp2.Text = (Convert.ToInt16(txtp2.Text) - Convert.ToInt16(txtq.Text)).ToString();
+                        checkedListBox1.Items.Add(label2.Text);
+                    }
+                    else if (Convert.ToInt16(txtp2.Text) < Convert.ToInt16(txtq.Text))
+                    {
+                        txtp2.Text = "0";
+                        checkedListBox1.Items.Add(label2.Text);
+                    }
+                }
+
+                //p3
+                if (Convert.ToInt16(txtp3.Text) > 0)
+                {
+                    if (Convert.ToInt16(txtp3.Text) >= Convert.ToInt16(txtq.Text))
+                    {
+                        txtp3.Text = (Convert.ToInt16(txtp3.Text) - Convert.ToInt16(txtq.Text)).ToString();
+                        checkedListBox1.Items.Add(label3.Text);
+                    }
+                    else if (Convert.ToInt16(txtp3.Text) < Convert.ToInt16(txtq.Text))
+                    {
+                        txtp3.Text = "0";
+                        checkedListBox1.Items.Add(label3.Text);
+                    }
+                }
+
+                //p4
+                if (Convert.ToInt16(txtp4.Text) > 0)
+                {
+                    if (Convert.ToInt16(txtp4.Text) >= Convert.ToInt16(txtq.Text))
+                    {
+                        txtp4.Text = (Convert.ToInt16(txtp4.Text) - Convert.ToInt16(txtq.Text)).ToString();
+                        checkedListBox1.Items.Add(label4.Text);
+                    }
+                    else if (Convert.ToInt16(txtp4.Text) < Convert.ToInt16(txtq.Text))
+                    {
+                        txtp4.Text = "0";
+                        checkedListBox1.Items.Add(label4.Text);
+                    }
+                }
+                textBox5.Text = "";
+                timer2.Start();
+
+            }
+            else
+            {
+                MessageBox.Show("You should Inter Values First");
+            }         
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (checkedListBox1.Items.Count>0)
+            {
+                textBox5.Text=checkedListBox1.Items[0].ToString();
+                checkedListBox1.Items.RemoveAt(0);
+                //timer2.Stop();
+            }
+            else if(checkedListBox1.Items.Count<=0)
+            {
+                timer2.Stop();
+                clear();               
+            }                
+        }
+    }
+}
